d4c811c0017772a4075d4b97a31bbf88abc4a457
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-05-30 12:45:06 +0100

    adds checkerboard in experiment.py

--- src/stimuli/__init__.py
+++ src/stimuli/__init__.py
@@ -1,4 +1,5 @@
 from .stimulus import Stimulus, playStimulus
 from .grating import DriftingGrating, StaticGrating
 from .sparse_noise import SparseNoise
+from .checkerboard import Checkerboard
 from .movie import Movie
